9727c7dac9ed144f285e0415e26098cf226d6982
SHED <281760739@qq.com>
2022-09-14 08:27:59 +0800

    Create

--- api/index.php
+++ api/index.php
@@ -1,4 +1,16 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+$str = file_get_contents('https://enka.network/u/'.$_GET['uid']);
+$str2 = '<img src="/ui/UI_AvatarIcon_Klee.png" alt="" class="svelte-qvi7br">';
+
+$str1 = 'http://baidu.com';
+
+$pattern = '/\/ui\/.*\.png/';//需要转义/
+
+preg_match($pattern,$str,$match);
+
+$a1 = $match[0];
+$a2 = 'https://enka.network'.$a1;
+echo $a2;
+
+header("Location: https://enka.network".$a1);
