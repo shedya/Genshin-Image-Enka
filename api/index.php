14d098b12fcd7f0dfaf8d3c54331e01ce227dcb6
SHED <281760739@qq.com>
2022-09-15 11:28:37 +0800

    OK

--- api/index.php
+++ api/index.php
@@ -1,19 +1,11 @@
 <?php
+$file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
 
-error_reporting (E_PARSE);
-
-ob_start();
-
-$str = file_get_contents('https://enka.network/u/'.$_GET['uid']);
-
-$pattern = '/\/ui\/.*\.png/';//需要转义/
-
-preg_match($pattern,$str,$match);
-
-$a1 = $match[0];
-$a2 = str_replace('/ui/','',$a1);
-$a3 = 'https://upload-bbs.mihoyo.com/game_record/genshin/character_side_icon/'.$a2;
-
-echo $a3;
-
-header("Location: ".$a3);
+if(file_exists($file))
+{
+   return false;
+}
+else
+{
+    require_once __DIR__ . '/../index.php';
+}
